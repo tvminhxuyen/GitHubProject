acf04ca51aa405c9242c9c02e3c6a8883f2c8dc4
tvminhxuyen <tvminhxuyen@gmail.com>
2015-04-22 08:30:09 +0700

    add minh anh source

---
+++ Minh Anh shop/app/ko/shoppingCart/sampleProductCategories2.js
@@ -0,0 +1,255 @@
+﻿define(function() {
+    return [
+        {
+            "products": [
+                {
+                    "name": "Nano White",
+                    "price": 350000
+                },
+                {
+                    "name": "Soft White",
+                    "price": 380000
+                },
+				{
+                    "name": "Lyli White",
+                    "price": 48000
+                }
+            ],
+            "name": "Kem Face"
+        },
+        {
+            "products": [
+                {
+                    "name": "SRM Enice trắng da",
+                    "price": 85000
+                },
+                {
+                    "name": "SRM Enice trị mụn",
+                    "price": 85000
+                }
+          ],
+            "name": "Sữa rửa mặt"
+        },
+        {
+            "products": [
+                {
+                    "name": "Body kiss",
+                    "price": 180000
+                },
+                {
+                    "name": "Body Amir",
+                    "price": 300000
+                }
+            ],
+            "name": "Kem body"
+        },
+        {
+            "products": [
+                {
+                    "name": "18th century schooner",
+                    "price": 82.34
+                },
+                {
+                    "name": "1999 Yamaha Speed Boat",
+                    "price": 51.61
+                },
+                {
+                    "name": "HMS Bounty",
+                    "price": 39.83
+                },
+                {
+                    "name": "Pont Yacht",
+                    "price": 33.3
+                },
+                {
+                    "name": "The Mayflower",
+                    "price": 43.3
+                },
+                {
+                    "name": "The Queen Mary",
+                    "price": 53.63
+                },
+                {
+                    "name": "The Schooner Bluenose",
+                    "price": 34.0
+                },
+                {
+                    "name": "The Titanic",
+                    "price": 51.09
+                },
+                {
+                    "name": "The USS Constitution Ship",
+                    "price": 33.97
+                }
+            ],
+            "name": "Ships"
+        },
+        {
+            "products": [
+                {
+                    "name": "1950's Chicago Surface Lines Streetcar",
+                    "price": 26.72
+                },
+                {
+                    "name": "1962 City of Detroit Streetcar",
+                    "price": 37.49
+                },
+                {
+                    "name": "Collectable Wooden Train",
+                    "price": 67.56
+                }
+            ],
+            "name": "Trains"
+        },
+        {
+            "products": [
+                {
+                    "name": "1926 Ford Fire Engine",
+                    "price": 24.92
+                },
+                {
+                    "name": "1940 Ford Pickup Truck",
+                    "price": 58.33
+                },
+                {
+                    "name": "1940s Ford truck",
+                    "price": 84.76
+                },
+                {
+                    "name": "1954 Greyhound Scenicruiser",
+                    "price": 25.98
+                },
+                {
+                    "name": "1957 Chevy Pickup",
+                    "price": 55.7
+                },
+                {
+                    "name": "1958 Setra Bus",
+                    "price": 77.9
+                },
+                {
+                    "name": "1962 Volkswagen Microbus",
+                    "price": 61.34
+                },
+                {
+                    "name": "1964 Mercedes Tour Bus",
+                    "price": 74.86
+                },
+                {
+                    "name": "1980â€™s GM Manhattan Express",
+                    "price": 53.93
+                },
+                {
+                    "name": "1996 Peterbilt 379 Stake Bed with Outrigger",
+                    "price": 33.61
+                },
+                {
+                    "name": "Diamond T620 Semi-Skirted Tanker",
+                    "price": 68.29
+                }
+            ],
+            "name": "Trucks and Buses"
+        },
+        {
+            "products": [
+                {
+                    "name": "18th Century Vintage Horse Carriage",
+                    "price": 60.74
+                },
+                {
+                    "name": "1903 Ford Model A",
+                    "price": 68.3
+                },
+                {
+                    "name": "1904 Buick Runabout",
+                    "price": 52.66
+                },
+                {
+                    "name": "1911 Ford Town Car",
+                    "price": 33.3
+                },
+                {
+                    "name": "1912 Ford Model T Delivery Wagon",
+                    "price": 46.91
+                },
+                {
+                    "name": "1913 Ford Model T Speedster",
+                    "price": 60.78
+                },
+                {
+                    "name": "1917 Grand Touring Sedan",
+                    "price": 86.7
+                },
+                {
+                    "name": "1917 Maxwell Touring Car",
+                    "price": 57.54
+                },
+                {
+                    "name": "1928 Ford Phaeton Deluxe",
+                    "price": 33.02
+                },
+                {
+                    "name": "1928 Mercedes-Benz SSK",
+                    "price": 72.56
+                },
+                {
+                    "name": "1930 Buick Marquette Phaeton",
+                    "price": 27.06
+                },
+                {
+                    "name": "1932 Alfa Romeo 8C2300 Spider Sport",
+                    "price": 43.26
+                },
+                {
+                    "name": "1932 Model A Ford J-Coupe",
+                    "price": 58.48
+                },
+                {
+                    "name": "1934 Ford V8 Coupe",
+                    "price": 34.35
+                },
+                {
+                    "name": "1936 Chrysler Airflow",
+                    "price": 57.46
+                },
+                {
+                    "name": "1936 Mercedes Benz 500k Roadster",
+                    "price": 21.75
+                },
+                {
+                    "name": "1936 Mercedes-Benz 500K Special Roadster",
+                    "price": 24.26
+                },
+                {
+                    "name": "1937 Horch 930V Limousine",
+                    "price": 26.3
+                },
+                {
+                    "name": "1937 Lincoln Berline",
+                    "price": 60.62
+                },
+                {
+                    "name": "1938 Cadillac V-16 Presidential Limousine",
+                    "price": 20.61
+                },
+                {
+                    "name": "1939 Cadillac Limousine",
+                    "price": 23.14
+                },
+                {
+                    "name": "1939 Chevrolet Deluxe Coupe",
+                    "price": 22.57
+                },
+                {
+                    "name": "1940 Ford Delivery Sedan",
+                    "price": 48.64
+                },
+                {
+                    "name": "1941 Chevrolet Special Deluxe Cabriolet",
+                    "price": 64.58
+                }
+            ],
+            "name": "Vintage Cars"
+        }
+    ];
+});
---
+++ Minh Anh shop/app/shell.js
@@ -0,0 +1,19 @@
+﻿define(['plugins/router'], function (router) {
+    return {
+        router: router,
+        activate: function () {
+            return router.map([
+                { route: ['', 'home'],                          moduleId: 'hello/index',                title: 'Giới thiệu',            nav: 1 },
+                { route: 'view-composition',                    moduleId: 'viewComposition/index',      title: 'Liên hệ',               nav: true },
+                { route: 'modal',                               moduleId: 'modal/index',                title: 'Khuyến mãi',            nav: 3 },
+                { route: 'event-aggregator',                    moduleId: 'eventAggregator/index',      title: 'Nhà cung cấp',          nav: 2 },
+                { route: 'widgets',                             moduleId: 'widgets/index',              title: 'Khách hàng',            nav: true },
+                { route: 'master-detail',                       moduleId: 'masterDetail/index',         title: 'Nhập hàng',             nav: true },
+                { route: 'knockout-samples*details',            moduleId: 'ko/index',                   title: 'Xuất hàng',             nav: true },
+                { route: 'keyed-master-details/:id*details',    moduleId: 'keyedMasterDetail/master',   title: 'Keyed Master Detail',   hash: '#keyed-master-details/:id' }
+            ]).buildNavigationModel()
+              .mapUnknownRoutes('hello/index', 'not-found')
+              .activate();
+        }
+    };
+});
